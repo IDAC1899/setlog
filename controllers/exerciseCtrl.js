e05dd9ce5170c0dc369a9e11b75c7354e4db5819
IDAC1899 <isaaldaaysi2@gmail.com>
2026-08-23 10:14:52 +0300

    added exercise controller

---
+++ controllers/exerciseCtrl.js
@@ -0,0 +1,48 @@
+const Exercise = required('../models/exercise');
+
+const index = async (req, res) => {
+    try {
+        const exercises = await Exercises.find({ owner: req.session.user._id});
+
+        res.render('exercises/index.ejs', {
+            exercises: exercises,
+        });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
+};
+
+const create = async (req, res) => {
+    try {
+        req.body.owner = req.session.user._id;
+        await Exercise.create(req.body);
+
+        res.redirect('/exercise');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
+};
+        
+const deleteExercise = async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.exerciseId);
+
+        if (!exercise.owner.equals(req.session.user._id)) {
+            returns res.redirect('/exercises');
+        }
+        await exercise.deleteOne();
+
+        res.redirect('/exercises');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
+};
+
+module.exports = {
+    index,
+    create,
+    deleteExercise,
+};
