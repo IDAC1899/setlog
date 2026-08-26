0769dcebb2d24f912391611e69c08a78d7435183
IDAC1899 <isaaldaaysi2@gmail.com>
2026-08-27 01:20:22 +0300

    add workout count and quick actions to home page

--- controllers/pagesCtrl.js
+++ controllers/pagesCtrl.js
@@ -1,4 +1,11 @@
+const Workout = require('../models/workout');
+
 const home = async (req, res) => {
+  if (req.session.user) {
+    const workoutCount = await Workout.countDocuments({ owner: req.session.user._id });
+    return res.render('index.ejs', { workoutCount });
+  }
+
   res.render('index.ejs');
 };
 
