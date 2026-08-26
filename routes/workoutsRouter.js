1daccb2ab421717a5bf82b0d77ebf9a9f7b475a2
IDAC1899 <isaaldaaysi2@gmail.com>
2026-08-27 01:38:16 +0300

    added template picker to the homepage and wire route

--- routes/workoutsRouter.js
+++ routes/workoutsRouter.js
@@ -7,9 +7,11 @@ const workoutCtrl = require('../controllers/workoutCtrl');
 router.get('/', workoutCtrl.index);
 router.get('/new', workoutCtrl.newWorkout);
 router.post('/', workoutCtrl.create);
+router.post('/from-template', workoutCtrl.createFromTemplate);
 router.get('/:workoutId', workoutCtrl.show);
 router.get('/:workoutId/edit', workoutCtrl.edit);
 router.put('/:workoutId', workoutCtrl.update);
 router.delete('/:workoutId', workoutCtrl.deleteWorkout);
+
 
 module.exports = router;
